6703b5e50c370d5d92432cb259b6de645dcf310a
Bastian Baumeister <ich@bastianbaumeister.de>
2021-11-08 19:38:43 +0100

    initial commit

---
+++ external_config_boilerplate.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#
+
+# HowTo  
+# As a simple way to keep private data out of public code
+# repositories, define the data you need as variables in a file called
+# config.sh in the same directory as the script and place that file in
+# the same directory as the script.  
+# Use the code below to load config.sh and to provide defaults vor the
+# variables.  
+# Make sure, not exclude the config.sh from the repo (.gitignore).
+
+
+## config
+# general
+CURRENT_DIRECTORY="$(dirname $0)"
+CONFIG_FILE="$CURRENT_DIRECTORY/config.sh"
+if [ -f "$CONFIG_FILE" ]; then
+    . "$CONFIG_FILE"
+fi
+
+VAR1=${VAR1:-'var1 default'}
+VAR2=${VAR2:-'var2 default'}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Linux helpers
+Stuff that helps me work with and manage my Linux machine.
+
+## ZFS health check
+Based on https://calomel.org/zfs_health_check_script.html, this checks health of ZFS volumes and drives. On any faults sends email.
